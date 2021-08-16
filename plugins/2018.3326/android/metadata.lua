e5e35494a21df091611244166cebcb62f224b442
Vlad Shcherban <vlad@coronalabs.com>
2021-08-16 20:10:44 +0300

    Updating to 2.10.1

--- plugins/2018.3326/android/metadata.lua
+++ plugins/2018.3326/android/metadata.lua
@@ -3,18 +3,6 @@ local metadata =
 	plugin =
 	{
 		format = "jar",
-		manifest =
-		{
-			applicationChildElements =
-			{
-				[[
-        <!--suppress AndroidDomInspection -->
-        <activity android:name="com.flurry.android.FlurryFullscreenTakeoverActivity"
-            android:configChanges="keyboard|keyboardHidden|orientation|screenLayout|uiMode|screenSize|smallestScreenSize">
-        </activity>
-					]]
-			}
-		}
 	},
 }
 
--- plugins/2018.3326/iphone/metadata.lua
+++ plugins/2018.3326/iphone/metadata.lua
@@ -3,8 +3,8 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-		staticLibs = { 'APDFlurryAdapter', },
-		frameworks = { 'FlurrySDK', },
+		staticLibs = { },
+		frameworks = { },
 		frameworksOptional = {},
 	},
 }
